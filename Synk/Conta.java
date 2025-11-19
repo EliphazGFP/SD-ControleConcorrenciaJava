bd9410b7119e851dcf36bcfb154d85c9dcef56ba
EliphazGFP <eliphazgabriel.volcano@outlook.com>
2025-11-19 15:32:37 -0300

    Update Conta.java

--- Synk/Conta.java
+++ Synk/Conta.java
@@ -1,29 +1,27 @@
+package Synk;
+
 public class Conta {
-    
-    private double saldo = 0;
-    
-    /** Creates a new instance of Conta */
-    public Conta(double saldo) {
-        this.saldo = saldo;
-        System.out.println("Conta criada. Saldo inicial: R$"  saldo);
+    private double saldo;
+
+    public Conta(double saldoInicial) {
+        this.saldo = saldoInicial;
+        System.out.println("Conta criada. Saldo inicial: R$ " + saldo);
     }
-    
+
+    public synchronized void depositar(double valor) {
+        saldo += valor;
+        System.out.println(Thread.currentThread().getName() + " depositou " + valor + ". Saldo: " + saldo);
+    }
+
+    public synchronized void sacar(double valor) {
+        if (saldo >= valor) {
+            try { Thread.sleep(100); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+            saldo -= valor;
+            System.out.println(Thread.currentThread().getName() + " sacou " + valor + ". Saldo: " + saldo);
+        }
+    }
+
     public synchronized double getSaldo() {
         return saldo;
-    }
-    
-    public synchronized void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    
-    public synchronized double debitarValor(double valor) {
-        if (this.saldo < valor) {
-            System.out.println("Saldo insuficiente para saque para "+ Thread.currentThread().getName() );
-            return -1;
-        } else {
-            this.saldo -= valor;
-            System.out.println("Saque realizado com sucesso para "+ Thread.currentThread().getName() );
-            return this.saldo;
-        }
     }
 }
